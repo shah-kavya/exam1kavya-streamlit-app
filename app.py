ff17403bea1bbe1476e06fa5a040bd77a97fde5c
shah-kavya <137373636+shah-kavya@users.noreply.github.com>
2025-04-16 14:58:21 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from exam1part1_starter_kavya import run_part1
+from exam1part2_starter_kavya import run_part2
+
+st.title("Automobile Data Analysis")
+
+option = st.radio("Choose Part to Run:", ["Part 1: Data Cleaning & Prep", "Part 2: Visualization & Correlation"])
+
+if option == "Part 1: Data Cleaning & Prep":
+    run_part1()
+elif option == "Part 2: Visualization & Correlation":
+    run_part2()
